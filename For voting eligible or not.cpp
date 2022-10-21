f52b77c2ccfc2c2e9d1254d3db6a215ff0d5444e
JAGANNATH PURTY <108622995+Jagannathpurty@users.noreply.github.com>
2022-10-21 13:19:04 +0530

    eligible

---
+++ For voting eligible or not.cpp
@@ -0,0 +1,27 @@
+#include<stdio.h>
+int main(){
+	int age;
+	printf("Enter a persons ages\n");
+	scanf("%d",&age);
+	if(age>18){
+		printf("A person is eligible for voting\n");
+		
+	}
+	else{
+		printf("A person is not eigible for voting");
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
