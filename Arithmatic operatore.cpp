2d38c84589be151db1eae3566289c1a813180f91
JAGANNATH PURTY <108622995+Jagannathpurty@users.noreply.github.com>
2023-05-19 13:29:44 +0530

    Arithmatic operatore

---
+++ Arithmatic operatore.cpp
@@ -0,0 +1,20 @@
+// OPERATORES
+//Arithmatic operatore
+#include<stdio.h>
+int main(){
+	int a,b,sum,div,mul,mod,sub;
+	a=9,b=8;
+	sum=a+b;
+	div=a/b;
+	mul=a*b;
+	mod=a%b;
+	sub=a-b;
+	
+	printf("The sum is=%d\n",sum);
+		printf("The div is=%d\n",div);
+			printf("The mul is=%d\n",mul);
+				printf("The mod is=%d\n",mod);
+					printf("The sub is=%d\n",sub);
+	
+	return 0;
+}
