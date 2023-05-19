64187cc039d57634f42a4e70c66e0110d2d70be1
JAGANNATH PURTY <108622995+Jagannathpurty@users.noreply.github.com>
2023-05-19 13:27:52 +0530

    Assignment Operatore

---
+++ Assignment operatore.cpp
@@ -0,0 +1,20 @@
+//Assignment Operatore
+#include<stdio.h>
+
+int main(){
+	int a,b;
+	a=9,b=49;
+	a=b;
+	printf("%d\n",a);
+	b+=5;
+	printf("%d\n",b);
+	int x,y;
+	x=6,y=7;
+	x^=5;
+	printf("%d\n",x);
+
+
+	
+
+	return 0;
+}
