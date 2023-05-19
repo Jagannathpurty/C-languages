719ac8692d4854bd743528081c5c3a866c6e026b
JAGANNATH PURTY <108622995+Jagannathpurty@users.noreply.github.com>
2023-05-19 13:26:20 +0530

    comparison

---
+++ Comparision operatore.cpp
@@ -0,0 +1,36 @@
+//Comparision or relational operatore
+
+#include<stdio.h>
+int main(){
+	int a,b,result1,result2,result3,result4,result5,result6;
+	a=8,b=30;
+	result1=(a<b);
+	result2=(a>b);
+	result3=(a==b);
+	result4=(a!=b);
+	result5=(a<=b);
+	result6=(a>=b);
+	
+	//if the condition is true then return(1)  and else is false return(0)
+	printf("The result2 is %d\n",result2);
+	printf("The result3 is %d\n",result3);
+	printf("The result4 is %d\n",result4);
+	printf("The result5 is %d\n",result5);
+	printf("The result6 is %d",result6);
+	
+	
+
+
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	return 0;
+}
