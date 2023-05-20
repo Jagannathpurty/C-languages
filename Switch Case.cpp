4eb71ad3d1815c39e5b3765caa23b5cc56b48637
JAGANNATH PURTY <108622995+Jagannathpurty@users.noreply.github.com>
2023-05-20 08:11:51 +0530

    switch

---
+++ Switch Case.cpp
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main(){
+	char a;
+	printf("Enter any characters: \n");
+	scanf("%c",&a);
+	switch(a){
+		case 'A':
+		printf("Vowel");
+		break;
+		case 'E':
+		printf("Vowel");
+		break;
+		case 'I':
+		printf("Vowel");
+		break;
+		case 'O':
+		printf("Vowel");
+		break;
+		case 'U':
+		printf("Vowel");
+		
+		break;
+		
+		default:
+		
+		printf("It is not vowel");
+			
+	}
+
+	return 0;
+}
