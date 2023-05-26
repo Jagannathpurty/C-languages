c2f52e3d283551f037f5f85573542657ecc7a047
JAGANNATH PURTY <108622995+Jagannathpurty@users.noreply.github.com>
2023-05-26 22:57:34 +0530

    variable

---
+++ Variables/variables.cpp
@@ -0,0 +1,15 @@
+//variables declaration
+#include<stdio.h>
+
+int main(){	
+int a,b,c;
+int _a,_b,_c;
+_a=8,_b=6;
+_c=_a-_b;
+a=8,b=8;
+c=a+b;
+printf("The sum is=%d\n",c);
+printf("The substraction is=%d",_c);	
+	
+	return 0;
+}
